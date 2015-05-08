26e32f9d01366d3fed6e6ae63b63c514c06e5b02
maheshnl <maheshnilla123@gmail.com>
2015-05-08 11:56:04 -0400

    Employee java file

---
+++ mahi/Employee.java
@@ -0,0 +1,28 @@
+package com.online;
+
+public class Employee {
+
+	private int eno;
+	private String firstname;
+	private String lastname;
+	
+	public int getEno() {
+		return eno;
+	}
+	public void setEno(int eno) {
+		this.eno = eno;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+}
